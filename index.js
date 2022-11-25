b5160a8c4803940877440140c4896cd74626b81b
MARUF3470 <smmaruf25@gmail.com>
2022-11-25 20:38:58 +0600

    used saved in db done

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+// userName : sellDeDb
+//password: aDBBl44kiwdMe9rM
+
+app.get('/', (req, res) => {
+    res.send('Sell de server is running')
+})
+
+
+const uri = "mongodb+srv://sellDeDb:aDBBl44kiwdMe9rM@cluster0.z1xe9fw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db('sellDe').collection('users')
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+app.listen(port, () => console.log(`sell de server is running on port ${port}`))
